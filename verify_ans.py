92870ebb4b4450f7ee3d72f7f262c0c5f73814f7
kimbochen <chentenghung@gmail.com>
2022-10-19 16:46:45 -0700

    Added usage description in comments.

--- verify_ans.py
+++ verify_ans.py
@@ -4,14 +4,15 @@ from pathlib import Path
 import numpy as np
 import scipy.sparse as sp
 
+# Usage: python3 verify_ans.py [matrix_filename] [num_iterations]
+
 
 data = np.loadtxt(sys.argv[1])
 
 m, n = map(int, data[0, :2])
 idxs, val = data[1:, :2], data[1:, 2]
-idxs = idxs - 1
+idxs = idxs - 1  # Convert to zero-based indices
 row, col = idxs[:, 0], idxs[:, 1]
-
 
 mat = sp.coo_matrix((val, (row, col)), shape=(m, n))
 x = np.ones([m, 1], dtype=np.float64)
