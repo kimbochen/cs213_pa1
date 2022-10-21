225f104829c878b463e57592075a25c443f23ad6
kimbochen <chentenghung@gmail.com>
2022-10-20 17:55:28 -0700

    Completed first CSC seq algo. To be memory optimized.

--- CSCSeq.c
+++ CSCSeq.c
@@ -5,28 +5,56 @@
 
 
 void sparseMatVecMul(
-    int m, int n, int nnz, int *row, int *col, double *val, double *x, double *y, int n_thread
+    int *row, int *col, double *val, double *x, double *y,
+    int N, int nnz, int n_iter
 )
 {
-    int *row_t = col;
-    int *col_t = row;
-    int *csc = (int*) malloc((m + 1) * sizeof(int));
+    // Convert col array to CSC format
+    int *csc = calloc(N + 1, sizeof(int));
 
-    csc[0] = 0;
-    // csc[c] is the first index j where col_t[j] != c
-    for (int c = 1, j = 0; c < m + 1; c++) {
-        while (col_t[j] == c && j < nnz) {
-            j++;
-        }
-        csc[c] = j;
+    // Count the number of elements in each column: column i at col[i+1]
+    for (int i = 0; i < nnz; i++) {
+        csc[col[i]]++;
     }
 
-    for (int i = 0; i < m; i++) {
-        for (int j = csc[i]; j < csc[i + 1]; j++) {
-            y[i] += val[j] * x[row_t[j] - 1];
+    // Perform cumulative sum
+    for (int i = 2; i < N + 1; i++) {
+        csc[i] += csc[i - 1];
+    }
+
+    // Create a column major order row array
+    int *row_t = (int*) malloc(nnz * sizeof(int));
+    double *val_t = (double*) malloc(nnz * sizeof(double));
+
+    for (int c = 1, idx = 0; c < N + 1; c++) {
+        for (int i = 0; i < nnz; i++) {
+            if (col[i] == c) {
+                row_t[idx] = row[i] - 1;
+                val_t[idx] = val[i];
+                idx++;
+            }
         }
     }
 
+    // Perform matrix-vector multiplication
+    for (int k = 0; k < n_iter; k++) {
+        for (int c = 0; c < N; c++) {
+            for (int j = csc[c]; j < csc[c + 1]; j++) {
+                y[row_t[j]] += val_t[j] * x[c];
+            }
+        }
+        // Set output as next input and zero-out output vector
+        if (k < n_iter - 1) {
+            for (int i = 0; i < N; i++) {
+                x[i] = y[i];
+                y[i] = 0.0;
+            }
+        }
+    }
+
+    free(csc);    // Free memory for CSC array
+    free(row_t);  // Free memory for row_t array 
+    free(val_t);  // Free memory for val_t array 
 }
 
 
